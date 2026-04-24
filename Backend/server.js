e025c3f65bf396eede406e05b7f360a371492e8a
ramaka947-tech <ramaka947@gmail.com>
2026-04-24 21:54:41 +0000

    fix cors pour Vercel

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,10 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://red-product-xi.vercel.app',
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
